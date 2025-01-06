d7785223ada10b13c8e1d22d984df26584e480cd
Pratik Mabrukhane <pratikmabrukhane19@gmail.com>
2025-01-06 16:36:16 +0530

    initial commit

---
+++ find_length_of_list.py
@@ -0,0 +1,14 @@
+myList = [1,2,3,4,5,6,7,8,9,0]
+
+#Approach 1
+
+count = 0
+
+for i in myList:
+    count = count + 1
+
+print("Length is : ",count)
+
+#Approach 2
+
+print("length is : ",len(myList))
